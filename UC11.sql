4d924715220e480fb461c4ce1de145599ed14af1
riteshkhunte05 <riteshkhunte05@gmail.com>
2022-05-06 18:47:35 +0530

    Added: Add person to Friend and family

---
+++ UC11.sql
@@ -0,0 +1,2 @@
+insert into Address_book(firstname, lastname, city, state, zip, phone, email) values('Rajendra', 'Gund', 'Ahmednagr', 'Maharastra', 414501, 9604315270, 'rajendragund@gmail.com' );
+update  Address_book set type='Family' where id =5;
